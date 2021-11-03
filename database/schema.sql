776c35fd2726c90449cd3c8dbfb668aeb9757bbe
Yunyun Liu <celeph4is@gmail.com>
2021-11-03 11:47:43 -0700

    set up empty database

--- database/schema.sql
+++ database/schema.sql
@@ -1,23 +1,18 @@
 set client_min_messages to warning;
-
 -- DANGER: this is NOT how to do it in the real world.
 -- `drop schema` INSTANTLY ERASES EVERYTHING.
 drop schema "public" cascade;
-
 create schema "public";
 
-CREATE TABLE "public.users" (
+CREATE TABLE "users" (
 	"userId" serial NOT NULL,
 	"password" TEXT NOT NULL,
-	"email" TEXT NOT NULL,
+	"email" TEXT NOT NULL unique,
 	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
 ) WITH (
   OIDS=FALSE
 );
-
-
-
-CREATE TABLE "public.boards" (
+CREATE TABLE "boards" (
 	"boardId" serial NOT NULL,
 	"userId" integer NOT NULL,
 	"name" TEXT NOT NULL,
@@ -25,10 +20,7 @@ CREATE TABLE "public.boards" (
 ) WITH (
   OIDS=FALSE
 );
-
-
-
-CREATE TABLE "public.columns" (
+CREATE TABLE "columns" (
 	"boardId" integer NOT NULL,
 	"columnId" serial NOT NULL,
 	"name" TEXT NOT NULL,
@@ -36,54 +28,37 @@ CREATE TABLE "public.columns" (
 ) WITH (
   OIDS=FALSE
 );
-
-
-
-CREATE TABLE "public.cards" (
+CREATE TABLE "cards" (
 	"cardId" serial NOT NULL,
 	"name" TEXT NOT NULL,
 	"description" TEXT,
 	"columnId" integer NOT NULL,
 	"boardId" integer NOT NULL,
-	"createdAt" TIMESTAMP NOT NULL,
+	"createdAt" TIMESTAMP NOT NULL default now(),
 	CONSTRAINT "cards_pk" PRIMARY KEY ("cardId")
 ) WITH (
   OIDS=FALSE
 );
-
-
-
-CREATE TABLE "public.tags" (
+CREATE TABLE "tags" (
 	"tagId" serial NOT NULL,
-	"color" VARCHAR(255) NOT NULL,
+	"color" TEXT NOT NULL,
 	"text" TEXT,
 	"boardId" integer NOT NULL,
 	CONSTRAINT "tags_pk" PRIMARY KEY ("tagId")
 ) WITH (
   OIDS=FALSE
 );
-
-
-
-CREATE TABLE "public.tagsCards" (
+CREATE TABLE "tagsCards" (
 	"cardId" integer NOT NULL,
 	"tagId" integer NOT NULL,
 	CONSTRAINT "tagsCards_pk" PRIMARY KEY ("cardId","tagId")
 ) WITH (
   OIDS=FALSE
 );
-
-
-
-
 ALTER TABLE "boards" ADD CONSTRAINT "boards_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
-
 ALTER TABLE "columns" ADD CONSTRAINT "columns_fk0" FOREIGN KEY ("boardId") REFERENCES "boards"("boardId");
-
 ALTER TABLE "cards" ADD CONSTRAINT "cards_fk0" FOREIGN KEY ("columnId") REFERENCES "columns"("columnId");
 ALTER TABLE "cards" ADD CONSTRAINT "cards_fk1" FOREIGN KEY ("boardId") REFERENCES "boards"("boardId");
-
 ALTER TABLE "tags" ADD CONSTRAINT "tags_fk0" FOREIGN KEY ("boardId") REFERENCES "boards"("boardId");
-
 ALTER TABLE "tagsCards" ADD CONSTRAINT "tagsCards_fk0" FOREIGN KEY ("cardId") REFERENCES "cards"("cardId");
 ALTER TABLE "tagsCards" ADD CONSTRAINT "tagsCards_fk1" FOREIGN KEY ("tagId") REFERENCES "tags"("tagId");
