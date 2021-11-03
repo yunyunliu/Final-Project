c34a2100351e140ce919e28b57beab60bbd5c243
Yunyun Liu <celeph4is@gmail.com>
2021-11-03 12:12:15 -0700

    bootstrap css set up

--- client/pages/components/PageHeader.jsx
+++ client/pages/components/PageHeader.jsx
@@ -2,21 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const PageHeader = () => (
- <div>
-      <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/boards/id">board1</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-          </nav>
- </div>
+<nav className="navbar navbar-light bg-light">
+</nav>
 );
 
 export default PageHeader;
