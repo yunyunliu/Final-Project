c2ebbbfca573b16d0ab1f9bf1bb10c1b5719fb2e
Yunyun Liu <celeph4is@gmail.com>
2021-11-03 14:11:15 -0700

    figure out html and css for PageHeader component

--- client/pages/components/PageHeader.jsx
+++ client/pages/components/PageHeader.jsx
@@ -2,20 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const PageHeader = () => (
- <div className='page-header'>
-      <nav>
-          <ul >
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/boards/id">board1</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-          </nav>
+ <div className='page-header row '>
+   <div className='column-full teal-bg justify-center'>
+      <div className='brand flex'>
+        <img className='logo' src='./images/Trello.png' alt="logo"/>
+        <div className='header-text semi-bold'>
+          <h1 className="no-margin">Kanban</h1>
+          <h2 className='pink-text no-margin'>Visualize and organize</h2>
+        </div>
+      </div>
+   </div>
+    <nav className='top-nav'>
+        <ul className='top-list'>
+          <li className="nav-item">
+            <Link className='gray-text' to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className='gray-text' to="/logout">Log Out</Link>
+          </li>
+        </ul>
+      </nav>
  </div>
 );
 
