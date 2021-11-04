a8d7560f5a75c5c9f5fa82b7bcaf190321e31e6e
Yunyun Liu <celeph4is@gmail.com>
2021-11-04 01:34:21 +0000

    boards render correctly with data passed in as props

--- database/data.sql
+++ database/data.sql
@@ -1,1 +1,9 @@
-INSERT INTO "boards"
+INSERT INTO "users" ("email", "password")
+  VALUES ('test@test.com', 'pass')
+  RETURNING *;
+
+INSERT INTO "boards" ("userId", "name")
+  VALUES (1, 'code journal'),
+          (1, 'secret project'),
+          (1, 'typescript project')
+  RETURNING *;
