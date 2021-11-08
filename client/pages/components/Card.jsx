06314d9217c9aa0642dd2852cfd63ae5b24bff1b
Yunyun Liu <celeph4is@gmail.com>
2021-11-08 01:19:46 +0000

    endpoint to create card and front end api call to create card work; using dummy data; page does not update to match data

--- client/pages/components/Card.jsx
+++ client/pages/components/Card.jsx
@@ -2,6 +2,17 @@ import React, { useState } from 'react';
 
 const Card = ({ card }) => {
 
+  const handleAdd = async () => {
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: 'yunyun', description: 'learn javascript' })
+    };
+    const response = await fetch('/api/users/1/boards/12/col/33/cards', options);
+    const newCard = await response.json();
+    console.log('new card:', newCard);
+  };
+
   return (
     <div className='card blue-bg'>
       <div className='card-name pink-text'>
@@ -14,8 +25,10 @@ const Card = ({ card }) => {
       <div className='card-desc'>
         {card.description}
       </div>
-      <button type='button'><i className='fas fa-plus'></i> New Card</button>
-      </div>
+      <button type='button'
+        onClick={() => handleAdd()}
+        ><i className='fas fa-plus'></i> New Card</button>
+    </div>
   );
 };
 
