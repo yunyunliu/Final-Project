2083defab6a668b5a3237bf54d5f1be176da2e5c
Yunyun Liu <celeph4is@gmail.com>
2021-11-11 01:58:23 +0000

    add color select grid to submenu

--- client/pages/components/SubMenu.jsx
+++ client/pages/components/SubMenu.jsx
@@ -9,19 +9,23 @@ const SubMenu = ({ data }) => {
 
   return (
     <div className='sub-menu blue-bg'>
-      <form className='flex flex-col'>
-        <h1>Create Tag</h1>
-        <input placeholder='tag name' />
+      <div className='menu-header semi-bold'>
+        <button className='icon-btn'><i className="fas fa-arrow-left"></i></button>
+        Create Tag
+        <button className='icon-btn'><i className="fas fa-times"></i></button>
+      </div>
+      <form className='flex flex-col add-tag-form'>
+        <input className='tag-text-input' placeholder='tag name' />
         <h3>tag color</h3>
         <div className='flex color-row-top'>
           <button className='color-label purple' id='purple' type='button'></button>
           <button className='color-label yellow' id='yellow' type='button'></button>
           <button className='color-label blue' id='blue' type='button'></button>
         </div>
-        <div className='flex color-row-top'>
+        <div className='flex color-row-bot'>
           <button className='color-label red' id='red' type='button'></button>
           <button className='color-label orange' id='orange' type='button'></button>
-          <button className='color-label' id='other' type='button'></button>
+          <button className='color-label' id='other' type='button'>Other</button>
       </div>
       </form>
     </div>
