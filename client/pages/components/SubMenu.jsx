de33f11f787af80cae5670d4ce4ecdec494a6539
Yunyun Liu <celeph4is@gmail.com>
2021-11-11 01:26:58 +0000

    style submenu element

--- client/pages/components/SubMenu.jsx
+++ client/pages/components/SubMenu.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SubMenu = ({ data }) => {
-  return (
-    <div className='sub-menu blue'>
-      <ul className='no-bullets'>
-        {
-          data.map(tag => <li className={tag.color} key={tag.tagId}>{tag.text}</li>)
-        }
+  const [selected, setSelected] = useState(null);
+  const [name, setName] = useState('');
+  const check = (
+    <i className="fas fa-check"></i>
+  );
 
-      </ul>
+  return (
+    <div className='sub-menu blue-bg'>
+      <form className='flex flex-col'>
+        <h1>Create Tag</h1>
+        <input placeholder='tag name' />
+        <h3>tag color</h3>
+        <div className='flex color-row-top'>
+          <button className='color-label purple' id='purple' type='button'></button>
+          <button className='color-label yellow' id='yellow' type='button'></button>
+          <button className='color-label blue' id='blue' type='button'></button>
+        </div>
+        <div className='flex color-row-top'>
+          <button className='color-label red' id='red' type='button'></button>
+          <button className='color-label orange' id='orange' type='button'></button>
+          <button className='color-label' id='other' type='button'></button>
+      </div>
+      </form>
     </div>
   );
 };
