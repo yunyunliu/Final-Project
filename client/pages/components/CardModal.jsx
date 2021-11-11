b120ecdd32a08e3789fa9207253a38aac1a4dcc4
Yunyun Liu <celeph4is@gmail.com>
2021-11-11 22:46:48 +0000

    add dtag section to expanded card

--- client/pages/components/CardModal.jsx
+++ client/pages/components/CardModal.jsx
@@ -4,18 +4,23 @@ const CardModal = ({ data, setExpanded, setEdit }) => {
   return (
     <dialog className='card-modal flex' open>
       <h2 className='no-margin card-task-name'>{data.name}</h2>
+      <div className='expanded-tag-container flex'>
+          {
+          data.tags.map(tag => <div key={tag.tagId} className={`${tag.color} expanded-tags`}>{tag.text}</div>)
+          }
+        </div>
       <div className='card-modal-description width-100'>
         <h3 className='pink-text no-margin expanded-description'>Task Description:</h3>
         <div className='gray-text source-sans expanded-description'> {data.description}</div>
       </div>
       <div className='expanded-btns'>
        <button
-          className='card-btns blue-bg pink-text semi-bold'
+          className='form-btn'
           type='button'
           onClick={e => setExpanded(false)}> Close</button>
         <button
           type='button'
-          className='card-btns blue-bg pink-text semi-bold'
+          className='form-btn'
           onClick={() => {
             setExpanded(false);
             setEdit(true);
