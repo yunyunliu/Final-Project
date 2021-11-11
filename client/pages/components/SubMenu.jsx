21649c2acd9062158e544ca0c1605d48c3008a37
Yunyun Liu <celeph4is@gmail.com>
2021-11-11 02:18:38 +0000

    checkmark appear and disappear on click

--- client/pages/components/SubMenu.jsx
+++ client/pages/components/SubMenu.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
 
 const SubMenu = ({ data }) => {
+  const [purple, setPurple] = useState(false);
+  const [yellow, setYellow] = useState(false);
+  const [blue, setBlue] = useState(false);
+  const [red, setRed] = useState(false);
+  const [orange, setOrange] = useState(false);
   const [selected, setSelected] = useState(null);
+
   const [name, setName] = useState('');
   const check = (
     <i className="fas fa-check"></i>
@@ -15,18 +21,19 @@ const SubMenu = ({ data }) => {
         <button className='icon-btn'><i className="fas fa-times"></i></button>
       </div>
       <form className='flex flex-col add-tag-form'>
-        <input className='tag-text-input' placeholder='tag name' />
+        <input className='tag-text-input' placeholder='tag name' onChange={e => setName(e.target.value) }/>
         <h3>tag color</h3>
         <div className='flex color-row-top'>
-          <button className='color-label purple' id='purple' type='button'></button>
-          <button className='color-label yellow' id='yellow' type='button'></button>
-          <button className='color-label blue' id='blue' type='button'></button>
+          <button className='color-label purple' id='purple' type='button' onClick={() => setPurple(!purple)}>{purple ? check : null}</button>
+          <button className='color-label yellow' id='yellow' type='button' onClick={() => setYellow(!yellow)}>{yellow ? check : null}</button>
+          <button className='color-label blue' id='blue' type='button' onClick={() => setBlue(!blue)}>{blue ? check : null}</button>
         </div>
         <div className='flex color-row-bot'>
-          <button className='color-label red' id='red' type='button'></button>
-          <button className='color-label orange' id='orange' type='button'></button>
+          <button className='color-label red' id='red' type='button' onClick={() => setRed(!red)}>{red ? check : null}</button>
+          <button className='color-label orange' id='orange' type='button' onClick={() => setOrange(!orange)}>{orange ? check : null}</button>
           <button className='color-label' id='other' type='button'>Other</button>
       </div>
+      <button>Add Tag</button>
       </form>
     </div>
   );
