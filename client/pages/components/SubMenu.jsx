627cc888642e3dbc278820b97180aef45851a17c
Yunyun Liu <celeph4is@gmail.com>
2021-11-17 00:03:28 +0000

    save

--- client/pages/components/SubMenu.jsx
+++ client/pages/components/SubMenu.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+const colorList = ['green', 'light-blue', 'gray', 'blue', 'pink', 'purple', 'orange', 'yellow', 'red', 'none'];
+
 const SubMenu = ({ setMenu, setTags, tags }) => {
   const [color, setColor] = useState('');
   const [text, setText] = useState('');
@@ -27,17 +29,29 @@ const SubMenu = ({ setMenu, setTags, tags }) => {
   return (
     <div>
       <div className='sub-menu'>
-        <div className='menu-header semi-bold'>
-          <button className='icon-btn header-btn' type='button' onClick={() => setMenu(false)}><i className="fas fa-arrow-left"></i></button>
-          <h3 style={{ fontSize: 20, marginLeft: 40 }} className='no-margin'>Create Tag</h3>
-          {/* <button type='button' onClick={() => setMenu(false)} className='icon-btn header-btn'><i className="fas fa-times"></i></button> */}
-        </div>
         <div className='flex width-100'>
-          <label htmlFor='color-input' style={{ marginRight: 15, fontSize: 18 }} className='semi-bold'>Color</label>
-          <input type='color'
-            value={color}
-            className='teal-border-2'
-            onChange={e => setColor(e.target.value)} />
+          <div>
+            <label style={{ marginRight: 15, fontSize: 18 }} className='semi-bold' htmlFor='tag-name-input'>Text</label>
+            <input value={text}
+                id='tag-text-input'
+                className='tag-text-input'
+                onChange={e => setText(e.target.value)} />
+            <button type='button'
+              onClick={() => {
+                handleAddTag(text, color);
+              }}
+              className='form-btn add-tag-btn'>Add
+            </button>
+          </div>
+          <div>
+            <ul className='no-bullets color-list'>
+              {colorList.map((color, i) => (
+                <li key={i}>
+                  <button type='button' className={`color-btn ${color}`}></button>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
         <div className='expanded-tag-container flex' style={{ marginTop: 15 }}>
             {tags.length > 0
@@ -52,17 +66,7 @@ const SubMenu = ({ setMenu, setTags, tags }) => {
               : null}
           </div>
         <div className='flex width-100 tag-text-section'>
-          <label style={{ marginRight: 15, fontSize: 18 }} className='semi-bold' htmlFor='tag-name-input'>Text</label>
-          <input value={text}
-              id='tag-text-input'
-              className='tag-text-input'
-              onChange={e => setText(e.target.value)} />
-          <button type='button'
-            onClick={() => {
-              handleAddTag(text, color);
-            }}
-            className='form-btn add-tag-btn'>Add
-          </button>
+
         </div>
       </div>
     </div>
