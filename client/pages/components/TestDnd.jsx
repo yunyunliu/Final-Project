ce56149ed714418aebf28b57672cdeb86f4446ce
Yunyun Liu <celeph4is@gmail.com>
2021-12-08 02:49:55 +0000

    reordered items in list saved to state

--- client/pages/components/TestDnd.jsx
+++ client/pages/components/TestDnd.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
@@ -10,15 +10,23 @@ const pokedex = [
   { number: '039', name: 'Jigglypuff' }
 ];
 
-const styles = { width: 100, height: 40, border: '2px solid green', listStyleType: 'none' };
-
 const TestDnd = () => {
+  const [pokelist, setPokeList] = useState(pokedex);
+  const handleOnDragEnd = result => {
+    const sourceIndex = result.source.index;
+    const destIndex = result.destination.index;
+    const pokeId = result.draggableId;
+    const copy = pokelist.slice();
+    const [moved] = copy.splice(sourceIndex, 1); // splice returns an array containing removed items
+    copy.splice(destIndex, 0, moved);
+    setPokeList(copy);
+  };
   return (
-    <DragDropContext>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
       <Droppable droppableId='poke'>
         {({ droppableProps, innerRef, placeholder }) => (
           <ul className='poke no-bullets' {...droppableProps} ref={innerRef}>
-            {pokedex.map((poke, i) => (
+            {pokelist.map((poke, i) => (
                              <Draggable key={poke.number} draggableId={poke.number} index={i}>
                                 {({ innerRef, draggableProps, dragHandleProps }) => (
                                 <li className='teal-border-2' ref={innerRef} {...draggableProps} {...dragHandleProps}>
