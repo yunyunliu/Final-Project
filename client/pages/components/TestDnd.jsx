09d26c33554ccc1ed3afc775c2e27c538c93ced2
Yunyun Liu <celeph4is@gmail.com>
2021-12-08 05:31:03 +0000

    dnd btw 2 lists working in test component

--- client/pages/components/TestDnd.jsx
+++ client/pages/components/TestDnd.jsx
@@ -2,31 +2,69 @@ import React, { useState } from 'react';
 
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-const pokedex = [
-  { number: '001', name: 'Bulbasaur' },
-  { number: '004', name: 'Charmander' },
-  { number: '007', name: 'Squirtle' },
-  { number: '025', name: 'Pikachu' },
-  { number: '039', name: 'Jigglypuff' }
-];
-
 const TestDnd = () => {
-  const [pokelist, setPokeList] = useState(pokedex);
+  const [dex, setDex] = useState({
+    kanto: [
+      { number: '001', name: 'Bulbasaur', order: 1 },
+      { number: '004', name: 'Charmander', order: 2 },
+      { number: '007', name: 'Squirtle', order: 3 },
+      { number: '025', name: 'Pikachu', order: 4 },
+      { number: '039', name: 'Jigglypuff', order: 5 }
+    ],
+    sinnoh: [
+      { number: '387', name: 'Turtwig', order: 1 },
+      { number: '390', name: 'Chimchar', order: 2 },
+      { number: '393', name: 'Piplup', order: 3 },
+      { number: '397', name: 'Staravia', order: 4 },
+      { number: '399', name: 'Bidoof', order: 5 }
+    ]
+  });
+
   const handleOnDragEnd = result => {
-    const sourceIndex = result.source.index;
-    const destIndex = result.destination.index;
-    const pokeId = result.draggableId;
-    const copy = pokelist.slice();
-    const [moved] = copy.splice(sourceIndex, 1); // splice returns an array containing removed items
-    copy.splice(destIndex, 0, moved);
-    setPokeList(copy);
+    const { destination, source, draggableId } = result;
+    if (!destination) {
+      return;
+    }
+    // console.log('results:', result)
+    const sourceI = source.index;
+    const destI = destination.index;
+    const itemId = draggableId;
+    if (destination.droppableId === source.droppableId) {
+      // move card within same column
+      const list = dex[destination.droppableId].slice();
+      const [reordered] = list.splice(sourceI, 1);
+      list.splice(destI, 0, reordered);
+      setDex({ ...dex, [destination.droppableId]: list });
+    } else {
+      // move card between columns
+      const srcList = dex[source.droppableId].slice();
+      const [moved] = srcList.splice(sourceI, 1);
+      const destList = dex[destination.droppableId].slice();
+      destList.splice(destI, 0, moved);
+      setDex({ [source.droppableId]: srcList, [destination.droppableId]: destList });
+    }
   };
+
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
-      <Droppable droppableId='poke'>
+      <Droppable droppableId='kanto'>
         {({ droppableProps, innerRef, placeholder }) => (
-          <ul className='poke no-bullets' {...droppableProps} ref={innerRef}>
-            {pokelist.map((poke, i) => (
+          <ul className='kanto no-bullets' {...droppableProps} ref={innerRef}>
+            {dex.kanto.map((poke, i) => (
+                             <Draggable key={poke.number} draggableId={poke.number} index={i}>
+                                {({ innerRef, draggableProps, dragHandleProps }) => (
+                                <li className='teal-border-2' ref={innerRef} {...draggableProps} {...dragHandleProps}>
+                                {poke.name}
+                                </li>)}
+                              </Draggable>))}
+                                  {placeholder}
+          </ul>
+        )}
+      </Droppable>
+      <Droppable droppableId='sinnoh'>
+        {({ droppableProps, innerRef, placeholder }) => (
+          <ul className='sinnoh no-bullets' {...droppableProps} ref={innerRef}>
+            {dex.sinnoh.map((poke, i) => (
                              <Draggable key={poke.number} draggableId={poke.number} index={i}>
                                 {({ innerRef, draggableProps, dragHandleProps }) => (
                                 <li className='teal-border-2' ref={innerRef} {...draggableProps} {...dragHandleProps}>
