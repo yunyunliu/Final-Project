08aa0cd7fe14908c00c9c3d4d426cd0b4160b64f
Yunyun Liu <celeph4is@gmail.com>
2022-03-05 20:19:11 -0800

    add dummy data

--- database/data.sql
+++ database/data.sql
@@ -5,7 +5,7 @@ INSERT INTO "users" ("email", "password")
 INSERT INTO "boards" ("userId", "name")
   VALUES  (1, 'typescript project'),
           (1, 'code journal' ),
-          (1, 'ajax project')
+          (1, 'recipe card app')
   RETURNING *;
 
 
@@ -22,12 +22,16 @@ INSERT INTO "columns" ("boardId", "name")
 
 INSERT INTO "cards" ("columnId", "boardId", "name", "description", "sequenceNum")
     VALUES (1, 3, 'style confirm modal', 'use a div with position:fixed', 0),
-          (1, 3, 'add react context', 'decide where what contexts are needed and where to put provider components', 1),
+          (1, 3, 'add react context', 'decide where what contexts are needed and where to put provider components. or use redux instead', 1),
           (1, 3, 'implement drag and drop', 'research html drag and drop api', 2),
           (2, 3, 'create endpoints', '', 0),
           (2, 3, 'change buttons in EditForm and AddForm to type submit', 'need to be type submit for required boolean attribute to work', 1),
           (2, 3, 'create Notification component for displaying error messages', '', 2),
-          (3, 3, 'trap focus on modals', 'do I need a library? focus-trap library?', 3)
+          (3, 3, 'trap focus on modals', 'do I need a library? focus-trap library?', 3),
+          (2, 3, 'build out authentication', 'figure out how OAuth works', 3),
+          (1, 3, 'find a data visualization library', 'which one is easier to use, chartjs or d3?', 3),
+          (1, 3, 'refactor backend to use sequelize', '', 3),
+          (1, 3, 'create app draw component for navigation')
 RETURNING *;
 
 INSERT INTO "tags" ("boardId", "text", "color")
