8a723bfc6ad02e1b1aaa72d5a8e6db1d00b00a2e
Yunyun Liu <celeph4is@gmail.com>
2022-03-05 18:26:47 -0800

    remove unnecessary comments

--- client/app.jsx
+++ client/app.jsx
@@ -12,16 +12,13 @@ import BoardView from './pages/BoardView';
 const App = () => {
   return (
     <Router>
-      {/* stuff outside Switch will appear in every view */}
       <PageHeader />
       <Switch>
-        {/* swappable views go here */}
         <Route path='/boards/:boardId'>
           <BoardView />
         </Route>
         <Route path='/'>
           <HomeView />
-          {/* <BoardView /> */}
         </Route>
       </Switch>
     </Router>
